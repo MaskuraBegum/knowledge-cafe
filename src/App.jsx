ed5e2c19a05b96584f2a23dfeedcdb2e0fc17332
MaskuraBegum <maskura.begum77@gmail.com>
2024-03-15 22:33:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Components/Header/Header'
+import './App.css'
+import Blog from './Components/Blogs/Blog'
+
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Blog></Blog>
+    </>
+  )
+}
+
+export default App
